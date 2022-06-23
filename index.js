81e995f3bdd8615eb34af93d3353d6090c4b9438
Nitesh Jha <nitesh.jha@buyerassist.io>
2022-06-24 01:58:13 +0530

    Move function to index file

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-console.log("FO!");
+const data = require("./data/get-relationship.json");
+const jp = require("jsonpath");
 
+const validateJsonPathArgument = (path, data) => {
+  if (!path && !data) {
+    throw new Error("path and data are empty!");
+  }
+  if (!path) {
+    throw new Error("path is empty!");
+  }
+  if (!data) {
+    throw new Error("data is empty!");
+  }
+  return { path, data };
+};
+
+const fetchValueFromPath = (pathValue, dataValue) => {
+  const { path, data } = validateJsonPathArgument(pathValue, dataValue);
+  const nodes = jp.apply(dataValue, pathValue, function (value) {
+    return value;
+  });
+  console.log("nodes", nodes);
+  return nodes;
+};
+
+module.exports = {
+  validateJsonPathArgument,
+  fetchValueFromPath,
+};
--- index.test.js
+++ index.test.js
@@ -1,18 +1,6 @@
 const data = require("./data/get-relationship.json");
 const jp = require("jsonpath");
-
-const validateJsonPathArgument = (path, data) => {
-  if (!path && !data) {
-    throw new Error("path and data are empty!");
-  }
-  if (!path) {
-    throw new Error("path is empty!");
-  }
-  if (!data) {
-    throw new Error("data is empty!");
-  }
-  return { path, data };
-};
+const { validateJsonPathArgument, fetchValueFromPath } = require("./index");
 
 describe("json path argument validation:", () => {
   test("has valid data directory ", () => {
@@ -44,15 +32,6 @@ describe("json path argument validation:", () => {
     });
   });
 });
-
-const fetchValueFromPath = (pathValue, dataValue) => {
-  const { path, data } = validateJsonPathArgument(pathValue, dataValue);
-  const nodes = jp.apply(dataValue, pathValue, function (value) {
-    return value;
-  });
-  console.log("nodes", nodes);
-  return nodes;
-};
 
 describe("fetch value via json path", () => {
   test("has path undefined will return empty array ", () => {
